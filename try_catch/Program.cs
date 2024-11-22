7fa2a09f53ab565c8b4fb8918a83ee60457b95db
Denis666-ctrl <ddneprovskij02@gmail.com>
2024-11-23 00:05:53 +0900

    Initial commit

---
+++ try_catch/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.Write("Введите первое число: ");
+            double x = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите второе число: ");
+            double y = int.Parse(Console.ReadLine());
+
+            double result = x / y;
+            Console.WriteLine($"{x}/{y}={result}");
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Ошибка: Деление на ноль невозможно.");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Ошибка: Введите корректное целое число.");
+        }
+        finally
+        {
+            Console.WriteLine("Программа завершила выполнение.");
+        }
+    }
+}
